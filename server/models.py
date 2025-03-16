75ee80cd6029a94737ea7d6ac7a7c8bad1b0332b
Abdullahi <aadahiye@gmail.com>
2025-03-16 07:07:23 +0300

    made the necessary importation into models.py

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
